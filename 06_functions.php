1593ff3f3cbf2f6ab0c728f2a3c65c97b95ffe41
iMMoCodes <matti-makela@hotmail.com>
2022-05-24 22:52:50 +0300

    06_functions

---
+++ 06_functions.php
@@ -0,0 +1,24 @@
+<?php 
+function registerUser($email) {
+    echo "$email registered";
+}
+
+// registerUser('iMMo');
+
+function sum($n1 = 4,$n2 = 5) {
+    return $n1 + $n2;
+}
+
+$number = sum();
+// echo $number;
+
+$subtract = function($n1,$n2) {
+    return $n1 - $n2;
+};
+
+// echo $subtract(10,5);
+
+$multiply = fn($n1,$n2) => $n1 * $n2;
+
+echo $multiply(9,9);
+?>
